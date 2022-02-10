8018cf349ed110024504661a092722ba473d5a4e
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-02-10 12:03:04 -0800

    Working on auth to complete user CRUD then onto game CRUD

--- gamr-app/src/components/UserHome.jsx
+++ gamr-app/src/components/UserHome.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ViewGames from './ViewGames';
 import {
 	IoHome,
@@ -8,14 +9,32 @@ import {
 	IoPersonCircleOutline,
 	IoLogoInstagram,
 } from 'react-icons/io5';
+import { useParams } from 'react-router-dom';
 
 export default function UserHome(props) {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		const getUsers = async () => {
+			const res = await axios.get('http://localhost:8000/users');
+			setUsers(res.data);
+		};
+		getUsers();
+	}, [props.match.params.id]);
+
+	console.log(users);
 	return (
 		<div className='UserHome-container'>
 			<div className='userhome-div'>
 				<IoPersonCircleOutline size={300} color={'gray'} />
 			</div>
-			<ViewGames />;
+			<div className='userhome-userdetails'>
+				{/* {users.map((user) => (
+                    {user.username}
+                ))}
+                </div>			 */}
+			</div>
+			<ViewGames />
 		</div>
 	);
 }
