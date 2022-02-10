e54d30e05c4125369608610d3fe42e222297fd2a
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-02-10 12:48:20 -0800

    Made some changes to urls.py

--- gamr_django/gamr_django/urls.py
+++ gamr_django/gamr_django/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gamr.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('accounts.url'))
 ]
